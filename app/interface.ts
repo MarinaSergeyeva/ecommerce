90d9ca681e0328b1205af27c2be60b5964a9672a
Maryna Serheieva <morrrinad@gmail.com>
2024-04-01 16:14:55 +0200

    product card

--- app/interface.ts
+++ app/interface.ts
@@ -2,7 +2,17 @@ export interface SimplifiedProduct {
   _id: string;
   imageUrl: string;
   price: number;
+  name: string;
   slug: string;
   categoryName: string;
+}
+
+export interface fullProduct {
+  _id: string;
+  images: any;
+  price: number;
   name: string;
+  slug: string;
+  categoryName: string;
+  description: string;
 }
